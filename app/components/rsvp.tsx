1dd76d92852af0c6947c782fa3c671e5e9206d2b
Mithila91 <mithilas.blog@gmail.com>
2024-04-14 13:49:46 +0200

    Add form and emailjs

--- app/components/rsvp.tsx
+++ app/components/rsvp.tsx
@@ -1,32 +1,104 @@
-'use client'
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-import { useState } from 'react';
+"use client";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import React, { useRef, useState } from "react";
+
+import emailjs from "@emailjs/browser";
 
 const RSVP = () => {
-    const { ref, inView } = useInView({
-        triggerOnce: true, 
-    });
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+  });
 
-    const fadeInVariants = {
-        hidden: { opacity: 0 },
-        show: { opacity: 1, transition: { duration: 0.5 } },
+  const fadeInVariants = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 0.5 } },
+  };
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    // Your EmailJS service ID, template ID, and Public Key
+    const serviceId = process.env.NEXT_PUBLIC_SERVICE_ID;
+    const templateId = process.env.NEXT_PUBLIC_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
+
+    // Create a new object that contains dynamic template params
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: "Web Wizard",
+      message: message,
     };
 
-    return (
-        <div className="h-[50vh] font-heading flex pt-40 justify-center bg-blue-800">
-            <motion.h2 
-                className="text-white text-5xl" 
-                ref={ref} 
-                variants={fadeInVariants} 
-                initial="hidden" 
-                animate={inView ? "show" : "hidden"}
-            >
-                RSVP
-            </motion.h2>
+    // Send the email using EmailJS
+    emailjs
+      .send(serviceId!, templateId!, templateParams, publicKey!)
+      .then((response) => {
+        console.log("Email sent successfully!", response);
+        setName("");
+        setEmail("");
+        setMessage("");
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+      });
+  };
+  return (
+    <div className="bg-blue-800 py-6 flex justify-center items-center h-full">
+      <motion.div
+        className="p-12 bg-white rounded-3xl w-96"
+        ref={ref}
+        variants={fadeInVariants}
+        initial="hidden"
+        animate={inView ? "show" : "hidden"}
+      >
+        <div className="mb-7">
+          <h3 className="font-semibold text-2xl text-gray-800">RSVP</h3>
         </div>
-    );
+        <form className="space-y-6 pb-6" onSubmit={handleSubmit}>
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full px-4 py-3 bg-gray-200 focus:bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-purple-400"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-3 bg-gray-200 focus:bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-purple-400"
+            />
+          </div>
+          <div>
+            <textarea
+              placeholder="Message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              className="w-full px-4 py-3 bg-gray-200 focus:bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:border-purple-400"
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full flex justify-center bg-purple-800 hover:bg-purple-700 text-gray-100 p-3 rounded-lg tracking-wide font-semibold cursor-pointer transition ease-in duration-500"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </motion.div>
+    </div>
+  );
 };
 
 export default RSVP;
