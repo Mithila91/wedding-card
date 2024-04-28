acfa9958d0001f81d65038ba334c186748d8d66f
Mithila91 <mithilas.blog@gmail.com>
2024-04-28 19:59:30 +0200

    Add vector images

--- app/components/schedule.tsx
+++ app/components/schedule.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-
+import Image from "next/image";
 
 const draw = {
   hidden: { pathLength: 0, opacity: 0 },
@@ -32,30 +32,37 @@ const Timeline = () => {
     {
       time: "14.00",
       description: "wedding ceremony.",
+      image: "/images/cermony.png",
     },
     {
       time: "15.00-16.00",
       description: "Photo shoot for the newlyweds.",
+      image: "/images/camera.png",
     },
     {
       time: "15.00-16.00",
       description: "Cocktails & Mingle for the guests",
+      image: "/images/cocktails.png",
     },
     {
       time: "17.00-18.00",
       description: "Dinner is served.",
+      image: "/images/dinner.png",
     },
     {
       time: "19.00-20.00",
       description: "First dance & cake cutting.",
+      image: "/images/dance.png",
     },
     {
       time: "21.00-00.45",
-      description:"Party time!",
+      description: "Party time!",
+      image: "/images/party.png",
     },
     {
       time: "1.00",
-      description:"Shuttles back to town",
+      description: "Shuttles back to town",
+      image: "/images/shuttle.png",
     },
   ];
 
@@ -65,9 +72,9 @@ const Timeline = () => {
         Schedule
       </h2>
       <div className="container bg-gray-200 mx-auto w-full h-full">
-        <div className="relative wrap overflow-hidden p-10 h-full">
+        <div className="relative wrap overflow-hidden">
           <svg
-            className="absolute bottom-0 left-1/2 transform -translate-x-1/2 h-full w-1 mb-20"
+            className="absolute top-0 left-1/2 transform -translate-x-1/2 h-full w-1 mb-20"
             viewBox="0 0 1 1"
             preserveAspectRatio="none"
           >
@@ -80,8 +87,8 @@ const Timeline = () => {
               animate="visible"
             />
           </svg>
-          <div className="container mx-auto w-full h-full">
-            <div className="relative wrap overflow-hidden p-10 h-full">
+          <div className="container mx-auto md:w-1/2 h-full">
+            <div className="relative wrap overflow-hidden h-full">
               <motion.div
                 ref={ref}
                 variants={stagger}
@@ -93,20 +100,32 @@ const Timeline = () => {
                     key={index}
                     className={`flex ${
                       index % 2 === 0 ? "flex-row-reverse" : ""
-                    } mb-8`}
+                    } mb-4`}
                     variants={{
                       initial: { opacity: 0, y: 0 },
-                      animate: { opacity: 1, y: 20, transition: { duration: 0.5 } },
+                      animate: {
+                        opacity: 1,
+                        y: 20,
+                        transition: { duration: 0.5 },
+                      },
                     }}
                   >
                     <div className="w-1/2 mx-8"></div>
-                    <div className=" px-6 py-4">
-                      <h3 className="mb-3 font-bold text-forest-800 text-xl">
-                        {data.time}
-                      </h3>
-                      <p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-                        {data.description}
-                      </p>
+                    <div className="flex flex-col md:flex-row items-start md:items-center md:space-x-4 px-8 py-4">
+                      <Image
+                        src={data.image}
+                        alt={data.description}
+                        width={100}
+                        height={100}
+                      />
+                      <div>
+                        <h3 className="mb-3 font-bold text-forest-800 text-xl">
+                          {data.time}
+                        </h3>
+                        <p className="text-m uppercase leading-snug tracking-wide text-gray-900 text-opacity-100">
+                          {data.description}
+                        </p>
+                      </div>
                     </div>
                   </motion.div>
                 ))}
@@ -116,10 +135,11 @@ const Timeline = () => {
         </div>
       </div>
       <div className="md:w-1/2 w-[350px] flex justify-center mx-auto">
-      <p className="tracking-wider leading-loose text-2xl my-8 mb-20 font-heading">Ps.. Since you know us you know that 
-      nothing will be according to the schedule....</p>
+        <p className="tracking-wider leading-loose md:text-2xl text-xl md:my-40 my-20 font-heading">
+          Ps.. Since you know us you know that nothing will be according to the
+          schedule....
+        </p>
       </div>
-
     </>
   );
 };
